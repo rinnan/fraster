5ab5e4de31cd3885185c4625bb1421b4b2a0c2b5
rinnan <rinnan@u.washington.edu>
2019-11-15 11:11:54 -0500

    Updated parameter documentation

--- R/fmask.R
+++ R/fmask.R
@@ -10,38 +10,15 @@
 #'
 #' @param x Raster* object
 #' @param mask Raster* object or a Spatial* object from sf or sp classes
-#' @param field numeric or character. The value(s) to be transferred. This can
-#' be a single number, or a vector of numbers that has the same length as the
-#' number of spatial features (points, lines, polygons). If x is a Spatial*DataFrame,
-#' this can be the column name of the variable to be transferred. If missing, the
-#' attribute index is used (i.e. numbers from 1 to the number of features). You
-#' can also provide a vector with the same length as the number of spatial features,
-#' or a matrix where the number of rows matches the number of spatial features
-#' @param fun function or character. To determine what values to assign to cells
-#' that are covered by multiple spatial features. You can use functions such as
-#' min, max, or mean, or one of the following character values: 'first', 'last',
-#' 'count'. The default value is 'last'. In the case of SpatialLines*, 'length'
-#' is also allowed (currently for planar coordinate systems only).
-#'
-#' If x represents points, fun must accept a na.rm argument, either explicitly or
-#' through 'dots'. This means that fun=length fails, but fun=function(x,...)length(x)
-#' works, although it ignores the na.rm argument. To use the na.rm argument you can
-#' use a function like this:
-#' fun=function(x, na.rm)if (na.rm) length(na.omit(x)) else (length(x), or use a
-#' function that removes NA values in all cases, like this function to compute the
-#' number of unique values per grid cell "richness":
-#' fun=function(x, ...) {length(unique(na.omit(x)))} . If you want to count the
-#' number of points in each grid cell, you can use fun='count' or
-#' fun=function(x,...){length(x)}.
-#'
-#' You can also pass multiple functions using a statement like
-#' fun=function(x, ...) c(length(x),mean(x)), in which case the returned object
-#' is a RasterBrick (multiple layers).
-#' @param background numeric. Value to put in the cells that are not covered by
-#' any of the features of x. Default is NA
-#' @param by character.  The name of a column in `sf` by which to aggregate
-#' layers.  If set, fasterize will return a RasterBrick with as many layers
-#' as unique values of the `by` column.
+#' @param filename character. Optional output filename
+#' @param inverse logical. If TRUE, areas on mask that are _not_ the maskvalue are masked
+#' @param maskvalue numeric. The value in mask that indicates the cells of x that 
+#' should become updatevalue (default = NA)
+#' @param updatevalue numeric. The value that cells of x should become if they are 
+#' not covered by mask (and not NA)
+#' @param updateNA logical. If TRUE, NA values outside the masked area are also 
+#' updated to the the updatevalue (only relevant if the updatevalue is not NA
+#' @param ... additional arguments as in writeRaster
 #'
 #' @examples
 #'
